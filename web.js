39932f715ce90eab4fce3bcfad7db63a5d541263
Felipe Rezende <feliperezende.srz@gmail.com>
2020-04-22 18:59:05 -0300

    paths

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/src/index.html')); //aqui você define onde está o index.html da sua aplicação.
+app.use(express.static(__dirname + '/src/')); //aqui você define onde está o index.html da sua aplicação.
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+    res.sendFile(path.join(`${__dirname}/src/index.html`));
 });
 
 app.listen(process.env.PORT || 3000);
