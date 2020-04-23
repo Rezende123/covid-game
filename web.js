adae30c28b9ed494b1b3aa636a80051513b2fb8c
Felipe Rezende <feliperezende.srz@gmail.com>
2020-04-22 22:02:37 -0300

    Redefinindo caminho do build

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/src/')); //aqui você define onde está o index.html da sua aplicação.
+app.use(express.static(__dirname + '/dist/covid-game/')); //aqui você define onde está o index.html da sua aplicação.
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/src/index.html`));
+    res.sendFile(path.join(`${__dirname}/dist/covid-game/index.html`));
 });
 
 app.listen(process.env.PORT || 3000);
